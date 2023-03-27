b76bfa6af652c800f9918e54763e701c5f908160
Bigtoxa766 <bigtoxa766@gmail.com>
2023-03-28 01:54:15 +0300

    uodate task

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,15 +1,11 @@
-const MAIN_URL = 'https://restcountries.com/v3.1/all';
+const MAIN_URL = 'https://restcountries.com/v3.1/name/';
 
 export const fetchCountries = name =>
-  fetch(`${MAIN_URL}?fields=${name},capital,population,flags,languages`)
+  fetch(`${MAIN_URL}${name}?fields=name,capital,population,flags,languages`)
     .then(res => {
       if (!res.ok) {
         throw new Error(res.status)
       }
       return res.json();
     })
-    .then(country =>{ 
-      console.log(country)})
-    .catch(error => 
-    error
-  ) 
+ 
