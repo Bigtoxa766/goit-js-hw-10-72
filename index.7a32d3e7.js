007e64024c1cc749e975f26ad522aeabdf25bc08
Bigtoxa766 <113884978+Bigtoxa766@users.noreply.github.com>
2023-03-27 22:54:47 +0000

    Deploying to gh-pages from @ Bigtoxa766/goit-js-hw-10@b76bfa6af652c800f9918e54763e701c5f908160 🚀

--- index.7a32d3e7.js
+++
@@ -1,2 +0,0 @@
-!function(){var e=document.querySelector(".country-list"),n=e.previousElementSibling;e.nextElementSibling;n.addEventListener("input",handleInput)}();
-//# sourceMappingURL=index.7a32d3e7.js.map
--- index.ad5d03f1.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector(".country-list"),n=e.previousElementSibling;e.nextElementSibling;n.addEventListener("input",handleInput);
-//# sourceMappingURL=index.ad5d03f1.js.map
